5446fc4d7f477fe326c1b01fcf693769c5d00515
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-07 21:01:47 -0500

    work gallery

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ $(function() {
   var selectedClass = "";
   $(".fil-cat").click(function() {
     selectedClass = $(this).attr("data-rel");
-    $("#portfolio").fadeTo(600, 0.5);
+    $("#portfolio").fadeTo(400, 0.5);
     $("#portfolio div")
       .not("." + selectedClass)
       .fadeOut()
