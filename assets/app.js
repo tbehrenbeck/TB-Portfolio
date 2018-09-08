da794cf553a29e61b554b36b3c5d47da71f8ee27
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-07 20:24:40 -0500

    navbar and intro sections

--- assets/app.js
+++ assets/app.js
@@ -3,31 +3,21 @@ $(document).ready(function() {
   $(".sidenav").sidenav();
 });
 
-//gallery
-$(window).load(function() {
-  var $container = $(".portfolioContainer");
-  $container.isotope({
-    filter: "*",
-    animationOptions: {
-      duration: 750,
-      easing: "linear",
-      queue: false
-    }
-  });
-
-  $(".portfolioFilter a").click(function() {
-    $(".portfolioFilter .current").removeClass("current");
-    $(this).addClass("current");
-
-    var selector = $(this).attr("data-filter");
-    $container.isotope({
-      filter: selector,
-      animationOptions: {
-        duration: 750,
-        easing: "linear",
-        queue: false
-      }
-    });
-    return false;
+// Works Gallery
+$(function() {
+  var selectedClass = "";
+  $(".fil-cat").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#portfolio").fadeTo(600, 0.5);
+    $("#portfolio div")
+      .not("." + selectedClass)
+      .fadeOut()
+      .removeClass("scale-anm");
+    setTimeout(function() {
+      $("." + selectedClass)
+        .fadeIn()
+        .addClass("scale-anm");
+      $("#portfolio").fadeTo(300, 1);
+    }, 300);
   });
 });
