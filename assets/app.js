8fa4ce720f935d302916d13c0ef69b67b940ca0a
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-16 14:27:42 -0500

    particles js

--- assets/app.js
+++ assets/app.js
@@ -1,90 +1,133 @@
-// Navbar
-$(document).ready(function() {
-  $(".sidenav").sidenav();
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
 });
+*/
 
-// Works Gallery
-$(function() {
-  var selectedClass = "";
-  $(".fil-cat").click(function() {
-    selectedClass = $(this).attr("data-rel");
-    $("#portfolio").fadeTo(400, 0.5);
-    $("#portfolio div")
-      .not("." + selectedClass)
-      .fadeOut()
-      .removeClass("scale-anm");
-    setTimeout(function() {
-      $("." + selectedClass)
-        .fadeIn()
-        .addClass("scale-anm");
-      $("#portfolio").fadeTo(300, 1);
-    }, 300);
-  });
-});
+/* Otherwise just put the config content (json): */
 
-// Scroll Animation
+particlesJS(
+  "particles-js",
 
-$(document).ready(function() {
-  $("#homeLink").on("click", function(event) {
-    event.preventDefault();
-    var hash = this.hash;
-
-    if ($(hash).offset()) {
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
+  {
+    particles: {
+      number: {
+        value: 90,
+        density: {
+          enable: true,
+          value_area: 700
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
         },
-        800,
-        function() {}
-      );
-    }
-  });
-});
-$(document).ready(function() {
-  $("#aboutLink").on("click", function(event) {
-    event.preventDefault();
-    var hash = this.hash;
-
-    if ($(hash).offset()) {
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
+        polygon: {
+          nb_sides: 5
         },
-        800,
-        function() {}
-      );
-    }
-  });
-});
-$(document).ready(function() {
-  $("#worksLink").on("click", function(event) {
-    event.preventDefault();
-    var hash = this.hash;
-
-    if ($(hash).offset()) {
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100
+        }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
         },
-        800,
-        function() {}
-      );
-    }
-  });
-});
-$(document).ready(function() {
-  $("#contactLink").on("click", function(event) {
-    event.preventDefault();
-    var hash = this.hash;
-
-    if ($(hash).offset()) {
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
+        onclick: {
+          enable: true,
+          mode: "push"
         },
-        800,
-        function() {}
-      );
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true,
+    config_demo: {
+      hide_card: false,
+      background_color: "#b61924",
+      background_image: "",
+      background_position: "50% 50%",
+      background_repeat: "no-repeat",
+      background_size: "cover"
     }
-  });
-});
+  }
+);
---
+++ assets/portfolio.js
@@ -0,0 +1,101 @@
+// Paralax
+(function($) {
+  $(function() {
+    $(".parallax").parallax();
+  });
+})(jQuery);
+
+// Navbar
+$(document).ready(function() {
+  $(".sidenav").sidenav();
+});
+
+// Modal
+$(document).ready(function() {
+  $(".modal").modal();
+});
+
+// Works Gallery
+$(function() {
+  var selectedClass = "";
+  $(".fil-cat").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#portfolio").fadeTo(400, 0.5);
+    $("#portfolio div")
+      .not("." + selectedClass)
+      .fadeOut()
+      .removeClass("scale-anm");
+    setTimeout(function() {
+      $("." + selectedClass)
+        .fadeIn()
+        .addClass("scale-anm");
+      $("#portfolio").fadeTo(300, 1);
+    }, 300);
+  });
+});
+
+// Scroll Animation
+$(document).ready(function() {
+  $("#homeLink").on("click", function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+
+    if ($(hash).offset()) {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {}
+      );
+    }
+  });
+});
+$(document).ready(function() {
+  $("#aboutLink").on("click", function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+
+    if ($(hash).offset()) {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {}
+      );
+    }
+  });
+});
+$(document).ready(function() {
+  $("#worksLink").on("click", function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+
+    if ($(hash).offset()) {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {}
+      );
+    }
+  });
+});
+$(document).ready(function() {
+  $("#contactLink").on("click", function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+
+    if ($(hash).offset()) {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {}
+      );
+    }
+  });
+});
